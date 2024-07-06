cdbaa0d3378c5edd9fdca4f4fe5d591545f534b8
Gu1t <oliveirariosilva@hotmail.com>
2024-07-06 10:23:43 -0300

    Reestruturação

--- desafio.py
+++ desafio.py
@@ -21,7 +21,7 @@ def depositar(saldo, valor, extrato, /):
         extrato += f"Depósito:\tR$ {valor:.2f}\n"
         print("\n=== Depósito realizado com sucesso! ===")
     else:
-        print("\n@@@ Operação falhou! O valor informado é inválido. @@@")
+        print("\n@@@ Operação falhou! O valor inválido. @@@")
 
     return saldo, extrato
 
@@ -32,13 +32,13 @@ def sacar(*, saldo, valor, extrato, limite, numero_saques, limite_saques):
     excedeu_saques = numero_saques >= limite_saques
 
     if excedeu_saldo:
-        print("\n@@@ Operação falhou! Você não tem saldo suficiente. @@@")
+        print("\n@@@ Operação falhou! Você não Possui saldo suficiente. @@@")
 
     elif excedeu_limite:
-        print("\n@@@ Operação falhou! O valor do saque excede o limite. @@@")
+        print("\n@@@ Operação falhou! O valor excede o limite. @@@")
 
     elif excedeu_saques:
-        print("\n@@@ Operação falhou! Número máximo de saques excedido. @@@")
+        print("\n@@@ Operação falhou! Número de saques excedido. @@@")
 
     elif valor > 0:
         saldo -= valor
@@ -47,14 +47,14 @@ def sacar(*, saldo, valor, extrato, limite, numero_saques, limite_saques):
         print("\n=== Saque realizado com sucesso! ===")
 
     else:
-        print("\n@@@ Operação falhou! O valor informado é inválido. @@@")
+        print("\n@@@ Operação falhou! O valor inválido. @@@")
 
     return saldo, extrato
 
 
 def exibir_extrato(saldo, /, *, extrato):
     print("\n================ EXTRATO ================")
-    print("Não foram realizadas movimentações." if not extrato else extrato)
+    print("Não Houveram movimentações." if not extrato else extrato)
     print(f"\nSaldo:\t\tR$ {saldo:.2f}")
     print("==========================================")
 
@@ -64,16 +64,16 @@ def criar_usuario(usuarios):
     usuario = filtrar_usuario(cpf, usuarios)
 
     if usuario:
-        print("\n@@@ Já existe usuário com esse CPF! @@@")
+        print("\n@@@ CPF já Cadastrado! @@@")
         return
 
-    nome = input("Informe o nome completo: ")
-    data_nascimento = input("Informe a data de nascimento (dd-mm-aaaa): ")
-    endereco = input("Informe o endereço (logradouro, nro - bairro - cidade/sigla estado): ")
+    nome = input("nome completo: ")
+    data_nascimento = input("data de nascimento (dd-mm-aaaa): ")
+    endereco = input("endereço (logradouro, nro - bairro - cidade/sigla estado): ")
 
     usuarios.append({"nome": nome, "data_nascimento": data_nascimento, "cpf": cpf, "endereco": endereco})
 
-    print("=== Usuário criado com sucesso! ===")
+    print("=== Usuário Registrado com sucesso! ===")
 
 
 def filtrar_usuario(cpf, usuarios):
@@ -82,14 +82,14 @@ def filtrar_usuario(cpf, usuarios):
 
 
 def criar_conta(agencia, numero_conta, usuarios):
-    cpf = input("Informe o CPF do usuário: ")
+    cpf = input("Informe o CPF: ")
     usuario = filtrar_usuario(cpf, usuarios)
 
     if usuario:
         print("\n=== Conta criada com sucesso! ===")
         return {"agencia": agencia, "numero_conta": numero_conta, "usuario": usuario}
 
-    print("\n@@@ Usuário não encontrado, fluxo de criação de conta encerrado! @@@")
+    print("\n@@@ Usuário não encontrado, criação de conta encerrada! @@@")
 
 
 def listar_contas(contas):
@@ -118,12 +118,12 @@ def main():
         opcao = menu()
 
         if opcao == "1":
-            valor = float(input("Informe o valor do depósito: "))
+            valor = float(input("valor do depósito: "))
 
             saldo, extrato = depositar(saldo, valor, extrato)
 
         elif opcao == "2":
-            valor = float(input("Informe o valor do saque: "))
+            valor = float(input("valor do saque: "))
 
             saldo, extrato = sacar(
                 saldo=saldo,
@@ -154,7 +154,7 @@ def main():
             break
 
         else:
-            print("Operação inválida, por favor selecione novamente a operação desejada.")
+            print("Operação inválida, por favor selecione novamente.")
 
 
 main()
